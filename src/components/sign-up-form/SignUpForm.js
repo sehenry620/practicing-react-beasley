da3fed2bdd9c6412e795937ae7b196837209ebf3
Henry <henrstep@amazon.com>
2019-11-21 16:41:20 -0500

    update files

--- src/components/sign-up-form/SignUpForm.js
+++ src/components/sign-up-form/SignUpForm.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './SignUpForm.css'
 
-function SignUpForm(props){
+class SignUpForm extends Component {
+    constructor (props){
+        super(props)
+
+        this.state ={
+            attendee: {
+                email: '',
+                guests: 0,
+
+            }
+
+        }
+    }
+
+    handleChange = (event) => {
+        const { attendee } = {...this.state}
+        attendee[event.target.name] = event.target.value
+        this.setState({
+            attendee
+        })
+    }
+
+
+    render(){
     return(
-        <form className="SignUpForm">
-        <input name="email" placeholder="Email" type="email" />
-        <input name="guests" placeholder="# of guests" type="number" />
-        <button>RSVP</button>
-        </form>
-    )
+            <form className="SignUpForm">
+            <input 
+                name="email" 
+                placeholder="Email" 
+                type="email" 
+                value={this.state.attendee.email}
+                onChange={this.handleChange}
+            />
+            <input 
+                name="guests"
+                placeholder="# of guests" 
+                type="number" 
+                value={this.state.attendee.guests}
+                onChange={this.handleChange}
+             />
+            <button>RSVP</button>
+            </form>
+        )
+    }
 
 }
 
